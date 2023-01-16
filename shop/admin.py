d416847c9be484e21a06d07274539b44429d361c
jubin <sebastianjubinps@gmail.com>
2023-01-16 23:24:36 +0530

    first

---
+++ shop/admin.py
@@ -0,0 +1,12 @@
+from . models import *
+from django.contrib import admin
+
+# Register your models here.
+class catadmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(categ,catadmin)
+class prodadmin(admin.ModelAdmin):
+    list_display = ['name','slug','price','stock','img','available']
+    list_editable = ['price','stock','img','available']
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(products,prodadmin)
